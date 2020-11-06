2928719a771bde7162daa1d4aa1c608527ce6bf2
AzretRealme <azretovichh@gmail.com>
2020-11-07 05:50:53 +0600

    added guitar

---
+++ src/Guitar.java
@@ -0,0 +1,36 @@
+public class Guitar {
+    private String color;
+    private int stringCount = 6;
+    private int[] tensions = new int[6];
+    //private int settings;
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setStringCount(int stringCount) {
+        this.stringCount = stringCount;
+
+    }
+
+    public int getStringCount() {
+        return this.stringCount;
+    }
+
+    public void tune(int tensions) {
+        System.out.println(tensions);
+        //this.stringCount -= settings / 1;
+    }
+
+    public void Play() {
+        System.out.println("Guitar Play " + " Wide open" + " Hey You" + " Late Night" + " Magic");
+    }
+
+    public int Unsettle() {
+        return 0;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args){
+        Guitar Drake = new Guitar();
+        Drake.setColor("black");
+        Drake.setStringCount(5);
+        Drake.tune(5);
+        Drake.Play();
+
+        System.out.println(Drake.getColor());
+        System.out.println(Drake.getStringCount());
+        System.out.println(Drake.Unsettle());
+
+
+    }
+}
